99014e5d2a8da1635bd878cf5bd57eb693c14a51
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-09-29 17:19:24 +0200

    Es6 migrate
    Es6 migrate

---
+++ src/helpers.js
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import chalk from "chalk";
+
+export default {
+  new() {
+    // TODO
+    console.log('Cooked');
+    process.exit(1);
+  },
+  get() {
+    // TODO
+    console.log('Menu');
+    process.exit(1);
+  },
+  exit(showMenu) {
+    return inquirer.prompt([{
+      type: 'confirm',
+      name: 'exit',
+      message: 'Are you sure want to exit?',
+      default: true
+    }]).then(answer => {
+      if (answer.exit) {
+        console.log(chalk`{bold.blue Till next time! :)}`);
+        process.exit(1);
+      }
+      showMenu();
+    })
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import inquirer from 'inquirer';
+import chalk from "chalk";
+import { version } from '../package.json';
+
+import helpers from './helpers';
+
+function showMenu() {
+  console.log(chalk`
+    {bold.yellow
+      $$$$$$$\\  $$\\                              
+      $$  __$$\\ \\__|                             
+      $$ |  $$ |$$\\ $$$$$$$$\\ $$$$$$$$\\ $$$$$$\\  
+      $$$$$$$  |$$ |\\____$$  |\\____$$  |\\____$$\\ 
+      $$  ____/ $$ |  $$$$ _/   $$$$ _/ $$$$$$$ |
+      $$ |      $$ | $$  _/    $$  _/  $$  __$$ |
+      $$ |      $$ |$$$$$$$$\\ $$$$$$$$\\$$$$$$$ |
+      \\__|      \\__|\\________|\\________|\\_______|}
+    {bold.yellow
+      Welcom to Pizza Maker v${version}
+    }
+  `);
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'Choose option:',
+        choices: [
+          {
+            name: 'Cook a new pizza',
+            value: 'new'
+          },
+          {
+            name: 'Select from menu',
+            value: 'get'
+          },
+          new inquirer.Separator(),
+          {
+            name: chalk`{bold.red Exit}`,
+            value: 'exit'
+          }
+        ]
+      }
+    ])
+    .then(answers => {
+      return helpers[answers.option](showMenu);
+    });
+}
+
+export default showMenu().catch(err => {
+  console.log(chalk`{bold.red Something went wrong! :( \n ${err}}`);
+  process.exit(1);
+});
